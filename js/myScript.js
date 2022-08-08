20ccb04b05d99da72e580cab60c8ff3361325e46
OlegJak <100533386+OlegJak@users.noreply.github.com>
2022-08-08 14:19:54 +0300

    Add files via upload

--- js/myScript.js
+++ js/myScript.js
@@ -66,3 +66,75 @@ $(".section__5__button__left").click(function(){
         countSection_5--;
     }
 });
+
+
+
+let animatePosition__1 = $(".section__2").offset();
+
+let animatePosition__2 = $(".section__3").offset();
+
+
+let animatePosition__3 = $(".section__4").offset();
+
+
+let animatePosition__4 = $(".section__5").offset();
+
+let animatePosition__5 = $(".section__6").offset();
+
+
+
+
+
+
+
+$(window).resize(function() {
+    animatePosition__1 = $(".section__2").offset();
+    animatePosition__2 = $(".section__3").offset();
+    animatePosition__3 = $(".section__4").offset();
+    animatePosition__4 = $(".section__5").offset();
+    animatePosition__5 = $(".section__6").offset();
+});
+    
+console.log(animatePosition__2)
+
+
+$(".section__6__container").slideUp(0)
+
+$(document).scroll(function () { 
+    var screenTop = $(document).scrollTop();
+
+    if (screenTop >= animatePosition__1.top / 2){
+        $(".section__2__col__1").animate(
+            {left: "0px"},
+            1500);
+        $(".section__2__col__2").animate(
+            {opacity: "1"},
+            1500); 
+    };
+
+    if (screenTop >= animatePosition__2.top - 400){
+        $(".section__3__col__1").animate(
+            {left: "0px"},
+            1500);
+        $(".section__3__col__2").animate(
+            {right: "0px"},
+            1500);
+    };
+
+    if (screenTop >= animatePosition__3.top - 500){
+        $(".section__4__p").animate(
+            {opacity: "1"},
+            1500);
+    }
+
+    if (screenTop >= animatePosition__4.top - 500){
+        $(".section__5__contant").animate(
+            {left: "0"},
+            1500);
+    }
+    if (screenTop >= animatePosition__5.top - 500){
+        $(".section__6__container").slideDown(1500)
+    }
+});
+
+
